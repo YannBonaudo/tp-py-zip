afe9797dc4eacc1ffea140b681851e37ac87f5b5
rabire <rabireh@outlook.fr>
2023-01-05 15:36:40 +0100

    client 1 fn

--- client.py
+++ client.py
@@ -3,37 +3,41 @@ from kivy.uix.button import Button
 from ftplib import FTP
 from zipfile import ZipFile
 
-def lire_contenu_zip(): 
-    print("lire lire lire lire")
-    # Create a ZipFile Object and load sample.zip in it
-    with ZipFile("temp_server_file.zip", 'r') as zipObj:
-        # Get list of files names in zip
-        listOfiles = zipObj.namelist()
-        # Iterate over the list of file names in given list & print them
-        for elem in listOfiles:
-            print(elem)
-            if elem == "fichier/lorem.txt" :
-                print(zipObj.getinfo(elem))
+# connexion au serveur ftp
+# ftp = FTP('127.0.0.1' )  
+# ftp.login("user", "pass")
+# ftp.cwd('/')
 
-def telecharger_fichier(self):
+# def telecharger_fichier(self):
+#     # copie temporaire du zip sur le serveur ftp 
+#     with open("temp_server_file.zip", "wb") as file:
+#         ftp.retrbinary("RETR fichier.zip", file.write)
+
+# def deziper_fichier(): 
+#     # dezip le fichier temporaire recupere sur le serveur
+#     with ZipFile("temp_server_file.zip", 'r') as zip_content:
+#         file_list = zip_content.extractall()
+
+def handle_click(self):
     # connexion au serveur ftp
     ftp = FTP('127.0.0.1' )  
     ftp.login("user", "pass")
     ftp.cwd('/')
+
     # copie temporaire du zip sur le serveur ftp 
-    ftp.cwd("/")
     with open("temp_server_file.zip", "wb") as file:
         ftp.retrbinary("RETR fichier.zip", file.write)
-    ftp.quit()
-
-    lire_contenu_zip()
     
-
+    # dezip le fichier temporaire sur le serveur
+    with ZipFile("temp_server_file.zip", 'r') as zip_content:
+        file_list = zip_content.extractall()
+    
+    ftp.quit()
 
 class MonApplication(App):
     def build(self):
-        bouton = Button(text='Télécharger le fichier')
-        bouton.bind(on_release=telecharger_fichier)
+        bouton = Button(text='Recupere le fichier, l encypter et le renvoyer')
+        bouton.bind(on_release=handle_click)
         return bouton
 
 MonApplication().run()
